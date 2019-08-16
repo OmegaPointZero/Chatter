a4dfa9b9c2295acc9930ef16ee0702443b70a239
OmegaPointZero <joshua.erik.campbell@gmail.com>
2019-08-16 16:21:14 -0600

    Skeleton Server upload: first code commit

---
+++ app.js
@@ -0,0 +1,49 @@
+/* Express stuff*/
+const express = require('express');
+const session = require('express-session');
+const app = express();
+/* MongoDB stuff*/
+const mongo = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const morgan = require('morgan'); // Logging reqs
+/* Passport stuff */
+const passport = require('passport');
+const flash = require('connect-flash');
+
+require('dotenv').config();
+var cors = (function(req,res,next){
+    if (req.method === "OPTIONS") {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+      } else {
+        res.header('Access-Control-Allow-Origin', '*');
+      }
+    next();
+});
+
+app.use(cors);
+
+mongoose.connect(process.env.MONGOURL, {useNewUrlParser:true}).then(res=>console.log('Connected to Mongo Database')).catch(err=>console.log(`error: ${err}`))
+mongoose.set('useCreateIndex',true)
+require('./config/passport')(passport);
+app.use(morgan('dev'))
+app.use(express.static('public'));
+app.use(flash());
+
+app.use(session({secret:process.env.SESSION_SECRET,resave:true,saveUninitialized:true}))
+require('./app/routes.js')(app,passport);
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', 'views');
+app.set('view engine', 'html');
+
+app.use(function(req,res,next){
+    Board.find({}, function(err,boards){
+        res.status(404).render('404.ejs', {
+            allBoards: boards
+        })
+    });
+});
+
+app.listen(process.env.SERVER_PORT,function(){
+    console.log(`Server started, listening for requests on port ${process.env.SERVER_PORT}`)
+})
